6c2adf0847a5d399b70d9542ab0581796a572551
Dylan Modell <dylan.modell.promo@gmail.com>
2019-01-11 08:04:30 -0800

    screen name bloc, title builder, deletebtn builder, legibility

--- lib/src/blocs/options_card_bloc.dart
+++ lib/src/blocs/options_card_bloc.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
-import 'dart:async';
 
 //Eventually each complex enough component will have its own bloc
 //  and the OptionsCardBloc will simply assign inputs and outputs.
 //  For PoC this is okay because there is only one component.
 class OptionsCardBloc {
   //Users Color Choice
-  final _primaryColorSubject = BehaviorSubject<Color>(seedValue: Colors.blue[500]);
+  final _primaryColor = BehaviorSubject<Color>(seedValue: Colors.blue[500]);
   final _primaryColorName = BehaviorSubject<String>(seedValue: "Blue");
+  //Users name choice
+  final _screenName = BehaviorSubject<String>(seedValue: "This is a new screen");
 
   //Getter to sink of Color Stream
-  Function(Color) get changeColor => handleColorChange;
+  Function(Color) get changeColor => _handleColorChange;
 
   //Getter to stream of Color objects
-  ValueObservable<Color> get primaryColor => _primaryColorSubject.stream;
+  ValueObservable<Color> get primaryColor => _primaryColor.stream;
 
   //Getter to stream of Strings of names of Colors
   ValueObservable<String> get primaryColorName => _primaryColorName.stream;
 
+  //Getter to sink of name stream
+  Function(String) get changeScreenName => _screenName.sink.add;
+
+  //Getter to stream of names
+  ValueObservable<String> get screenName => _screenName.stream;
+
   void dispose(){
-    _primaryColorSubject.close();
+    _primaryColor.close();
     _primaryColorName.close();
+    _screenName.close();
   }
 
-  void handleColorChange(Color color){
-    _primaryColorSubject.sink.add(color);
+  void _handleColorChange(Color color){
+    _primaryColor.sink.add(color);
     if(color == Colors.blue[500]){
       _primaryColorName.sink.add("Blue");
     } else {
--- lib/src/widgets/options_card.dart
+++ lib/src/widgets/options_card.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import '../blocs/options_card_bloc.dart';
-import 'dart:async';
-
-//TODO: refactor for legibility
 
 class OptionsCard extends StatelessWidget{
   @override
@@ -12,54 +9,83 @@ class OptionsCard extends StatelessWidget{
     return Card(
       child: Column(
         children: <Widget>[
-          ListTile(
-            title: Text('This is a widget'),
-            subtitle: Text('Choose some options'),
-          ),
-
-          colorPicker(bloc),
-
-          ButtonTheme.bar(
-            child: ButtonBar(
-              children: <Widget>[
-                FlatButton(
-                  onPressed: () => print('Delete'),
-                  child: Text('Delete'),
-                ),
-              ],
-            ),
-          )
+          titleBuilder(bloc),
+          screenNameBuilder(bloc),
+          colorPickerBuilder(bloc),
+          deleteButton(),
         ],
       ),
     );
   }
 
-  Widget colorPicker(OptionsCardBloc bloc){
-    return StreamBuilder(
-      stream: bloc.primaryColor,
-      // initialData: bloc.primaryColor.value,
-      builder: (context, snapshot){
-        return DropdownButton<Color>(
-          hint: Text(bloc.primaryColorName.value),
-          onChanged: (chosenColor) {
-            bloc.changeColor(chosenColor);
-            print("The selected primary color is ${bloc.primaryColor.value}, "
-              "also known as ${bloc.primaryColorName.value}");
-          },
-          items: <DropdownMenuItem<Color>>[
-            DropdownMenuItem(
-              child: Text('Blue'),
-              value: Colors.blue[500],
-            ),
-            DropdownMenuItem(
-              child: Text('Red'),
-              value: Colors.red[500],
-            ),
-          ],
-        );
-      },
+  Widget deleteButton(){
+    return ButtonTheme.bar(
+      child: ButtonBar(
+        children: <Widget>[
+          FlatButton(
+            onPressed: () => print('Delete'),
+            child: Text('Delete'),
+          ),
+        ],
+      ),
     );
-    
-    
+  }
+
+  Widget titleBuilder(OptionsCardBloc bloc){
+    return StreamBuilder(
+      stream: bloc.screenName,
+      builder: (context, snapshot) => ListTile(
+        title: Text(bloc.screenName.value),
+        subtitle: Text('Choose some options to customize'),
+      ),
+    );
+  }
+
+  Widget colorPickerBuilder(OptionsCardBloc bloc){
+    return Container(padding: EdgeInsets.all(16.0), child: Row(
+      children: <Widget>[
+        Text("Primary color:    ", style: TextStyle(fontSize: 16.0)),
+        StreamBuilder(
+          stream: bloc.primaryColor,
+          initialData: bloc.primaryColor.value,
+          builder: (context, snapshot){
+            return DropdownButton<Color>(
+              hint: Text(bloc.primaryColorName.value),
+              onChanged: (chosenColor) => bloc.changeColor(chosenColor),
+              items: <DropdownMenuItem<Color>>[
+                DropdownMenuItem(
+                  child: Text('Blue'),
+                  value: Colors.blue[500],
+                ),
+                DropdownMenuItem(
+                  child: Text('Red'),
+                  value: Colors.red[500],
+                ),
+              ],
+            );
+          },
+        ),
+      ],
+    ));
+  }
+
+  Widget screenNameBuilder(OptionsCardBloc bloc){
+    return StreamBuilder(
+      stream: bloc.screenName,
+      initialData: bloc.screenName.value,
+      builder: (context, snapshot){
+        return Container(
+          padding: EdgeInsets.only(left: 16.0, right: 200.0),
+          child: TextField(
+            autofocus: false,
+            decoration: InputDecoration(
+              labelText: "Name the section",
+              hintText: "Screen 1",
+            ),
+            onChanged: (name) => bloc.changeScreenName(name),
+          ),
+        );
+      }
+    );
   }
 }
